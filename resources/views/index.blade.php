b063a6fcc463bd6f813c42c569c805053bdc7124
Obez99 <giuliogx@live.it>
2021-12-20 18:28:21 +0100

    Completes shop section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -23,7 +23,7 @@
         <div class="comics-container">
           <div class="row row-cols-6">
             @foreach ($comics_list as $comic)
-            <div class="comics-card">
+            <div class="col comics-card">
               <img src="{{$comic["thumb"]}}" alt="">
               <h6>{{$comic["series"]}}</h6>
             </div> 
@@ -31,6 +31,53 @@
           </div>
 
           <button>LOAD MORE</button>
+        </div>
+      </div>
+    </section>
+
+    <section class="shop-section">
+      <div class="container">
+        <div class="row row-cols align-items-center">
+          <div class="col">
+            <div class="shop-menu-item">
+              <a href="#">
+              <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+              <h6>DIGITAL COMICS</h6>
+              </a>
+            </div>
+          </div>
+          <div class="col">
+            <div class="shop-menu-item">
+              <a href="#">
+              <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="">
+              <h6>MERCHANDISE</h6>
+              </a>
+            </div>
+          </div>
+          <div class="col">
+            <div class="shop-menu-item">
+              <a href="#">
+              <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="">
+              <h6>SUBSCRIPTION</h6>
+              </a>
+            </div>
+          </div>
+          <div class="col">
+            <div class="shop-menu-item">
+              <a href="#">
+              <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="">
+              <h6>SHOP LOCATOR</h6>
+              </a>
+            </div>
+          </div>
+          <div class="col">
+            <div class="shop-menu-item">
+              <a href="#">
+              <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="">
+              <h6>POWER VISA</h6>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </section>
