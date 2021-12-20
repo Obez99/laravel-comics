0d2f057104429e1cdab1e92c67d6188203ba4176
Obez99 <giuliogx@live.it>
2021-12-20 18:08:18 +0100

    Adds comics section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,41 +8,31 @@
   @include('partials.head_scripts')
 </head>
 <body>
-  <header>
-    <div class="upper-header bg-primary">
-      <div class="container d-flex justify-content-end">
-        <a href="#">DC POWER VISA</a>
-        <button>ADDITIONAL DC SITES <i class="fas fa-caret-down"></i> </button>
-      </div>
-    </div>
 
-    <div class="bottom-header">
-      <div class="container">
-        <img src="{{asset('img/dc-logo.png')}}" alt="">
-        <ul>
-          <li><a href="#">CHARACTERS</a></li>
-          <li><a href="#">COMICS</a></li>
-          <li><a href="#">MOVIES</a></li>
-          <li><a href="#">TV</a></li>
-          <li><a href="#">GAMES</a></li>
-          <li><a href="#">COLLECTIBLES</a></li>
-          <li><a href="#">VIDEOS</a></li>
-          <li><a href="#">FANS</a></li>
-          <li><a href="#">NEWS</a></li>
-          <li><a href="#" class="custom-dropdown">SHOP <i class="fas fa-caret-down"></i></a></li>
-        </ul>
-        <input type="text" placeholder="Search" class="text-end">
-      </div>
-    </div>
-
-    <nav>
-
-    </nav>
-  </header>
+  @include('partials.header')
 
   <main>
     <section class="jumbo">
 
+    </section>
+
+    <section class="comics-section">
+      <div class="container">
+      <h4>CURRENT SERIES</h4>
+
+        <div class="comics-container">
+          <div class="row row-cols-6">
+            @foreach ($comics_list as $comic)
+            <div class="comics-card">
+              <img src="{{$comic["thumb"]}}" alt="">
+              <h6>{{$comic["series"]}}</h6>
+            </div> 
+            @endforeach
+          </div>
+
+          <button>LOAD MORE</button>
+        </div>
+      </div>
     </section>
   </main>
 </body>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,31 @@
+<header>
+  <div class="upper-header bg-primary">
+    <div class="container d-flex justify-content-end">
+      <a href="#">DC POWER VISA</a>
+      <button>ADDITIONAL DC SITES <i class="fas fa-caret-down"></i> </button>
+    </div>
+  </div>
+
+  <div class="bottom-header">
+    <div class="container">
+      <img src="{{asset('img/dc-logo.png')}}" alt="">
+      <ul>
+        <li><a href="#">CHARACTERS</a></li>
+        <li><a href="#">COMICS</a></li>
+        <li><a href="#">MOVIES</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">GAMES</a></li>
+        <li><a href="#">COLLECTIBLES</a></li>
+        <li><a href="#">VIDEOS</a></li>
+        <li><a href="#">FANS</a></li>
+        <li><a href="#">NEWS</a></li>
+        <li><a href="#" class="custom-dropdown">SHOP <i class="fas fa-caret-down"></i></a></li>
+      </ul>
+      <input type="text" placeholder="Search" class="text-end">
+    </div>
+  </div>
+
+  <nav>
+
+  </nav>
+</header>
